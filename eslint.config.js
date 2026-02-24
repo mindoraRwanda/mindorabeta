e5e7b10b869dcdbd1acfa0d7f2d0a4dbc78bc206
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2026-02-24 22:59:27 +0200

    eslint  update

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,7 @@ export default [
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: './tsconfig.json',
+        project: './tsconfig.eslint.json',
         tsconfigRootDir: import.meta.dirname,
       },
     },
