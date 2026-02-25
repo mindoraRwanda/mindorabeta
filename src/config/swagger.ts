c16fbd49667f3ba9f79227d630fa937998f4071c
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2026-02-26 00:14:37 +0200

    update swagger

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -14,7 +14,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: process.env.API_URL || 'http://localhost:5000',
+        url: (process.env.API_URL || 'http://localhost:5000') + '/api/v1',
         description: 'Development server',
       },
     ],
