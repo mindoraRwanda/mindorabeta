9c3f214f5d8e7f159b1f029496f9dac8429980d6
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2026-02-26 00:14:25 +0200

    add server handling

--- src/server.ts
+++ src/server.ts
@@ -38,4 +38,20 @@ const startServer = async () => {
   }
 };
 
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err: Error) => {
+  logger.error('UNHANDLED REJECTION! 💥 Shutting down...');
+  logger.error(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (err: Error) => {
+  logger.error('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  logger.error(err.name, err.message);
+  process.exit(1);
+});
+
 startServer();
